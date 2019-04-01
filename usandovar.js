56155c5a6b5ebf5e51ceb2f9980e397b784db153
gregolly <gregollyff@hotmail.com>
2019-04-01 05:13:17 +0000

    js

---
+++ usandovar.js
@@ -0,0 +1,18 @@
+{
+  {
+    {
+      {
+        var sera = "Será??"
+        console.log(sera);
+      }
+    }
+  }
+}
+
+console.log(sera + " Só imaginação");
+
+function teste() {
+  var local = 123;
+}
+teste()
+console.log(local)
